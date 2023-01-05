bd87abc380ec5fda82e52675ba6fde493b66e9f4
Konstantin <constantinopladwar@gmail.com>
2023-01-05 15:49:43 +0100

    3.1 Creo la clase Cliente con los atributos requeridos

---
+++ src/main/java/org/iesalandalus/programacion/clientesempresa/modelo/dominio/Cliente.java
@@ -0,0 +1,18 @@
+package org.iesalandalus.programacion.clientesempresa.modelo.dominio;
+
+import java.util.Date;
+
+public class Cliente {
+	
+	
+	private static final String ER_CORREO = "";
+	private static final String ER_DNI = "";
+	private static final String ER_TELEFONO = "";
+	public static final String FORMATO_FECHA = "";
+	private String nombre;
+	private String dni;
+	private String correo;
+	private String telefono;
+	private Date fechaNacimiento;
+	
+}
