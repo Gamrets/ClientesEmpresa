b3cb11279b2e78ada91f147d156aa2fd4c9667cc
Konstantin <constantinopladwar@gmail.com>
2023-01-06 12:12:03 +0100

    3.2 Creo metodo formateaNombre

--- src/main/java/org/iesalandalus/programacion/clientesempresa/modelo/dominio/Cliente.java
+++ src/main/java/org/iesalandalus/programacion/clientesempresa/modelo/dominio/Cliente.java
@@ -1,6 +1,7 @@
 package org.iesalandalus.programacion.clientesempresa.modelo.dominio;
 
 import java.util.Date;
+import java.util.StringTokenizer;
 
 public class Cliente {
 	
@@ -14,5 +15,29 @@ public class Cliente {
 	private String correo;
 	private String telefono;
 	private Date fechaNacimiento;
+	
+	
+	
+	private String formateaNombre(String nombre) {
+		
+		StringTokenizer st = new StringTokenizer(nombre);
+		StringBuilder stringBuilder = new StringBuilder();
+
+		while (st.hasMoreElements()) {
+
+			String siguienteElemento = (String) st.nextElement();
+
+			if (siguienteElemento.length() > 0) {
+
+				stringBuilder.append(siguienteElemento.substring(0, 1).toUpperCase());
+				stringBuilder.append(siguienteElemento.substring(1).toLowerCase());
+				stringBuilder.append(' ');
+			}
+		}
+
+		return stringBuilder.toString();
+	}
+	
+	
 	
 }
